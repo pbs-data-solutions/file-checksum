ba5ab952c9d7c953aaddf96c42353fa93ba39ddf
Paul Sanders <psanders1@gmail.com>
2023-04-11 15:33:04 -0400

    Add check sum generation

---
+++ file_checksum/__main__.py
@@ -0,0 +1,146 @@
+from __future__ import annotations
+
+from enum import Enum
+from hashlib import md5, sha1, sha256
+from pathlib import Path
+from typing import Union
+
+from rich.console import Console
+from rich.theme import Theme
+from typer import Argument, Exit, Option, Typer
+
+__version__ = "0.1.0"
+
+app = Typer()
+
+custom_style = Theme({"error": "red", "date": "green"})
+console = Console(theme=custom_style)
+
+
+class ChecksumType(str, Enum):
+    MD5 = "md5"
+    SHA1 = "sha1"
+    SHA256 = "sha256"
+
+
+class FileTypeError(Exception):
+    pass
+
+
+def _check_valid_output_file_type(file_name: Path) -> None:
+    if file_name.suffix != ".txt":
+        raise FileTypeError("Output file must be a .txt file")
+
+    return None
+
+
+def _process_checksum(
+    file_path: Path,
+    *,
+    output_file: Path | None = None,
+    checksum_type: ChecksumType = ChecksumType.SHA256,
+    verbose: bool = False,
+) -> None:
+    if not output_file:
+        verbose = True
+
+    try:
+        with open(file_path, "rb") as f:
+            if checksum_type == ChecksumType.SHA256:
+                file_hash = sha256(f.read())
+            elif checksum_type == ChecksumType.MD5:
+                file_hash = md5(f.read())
+            else:
+                file_hash = sha1(f.read())
+    except IsADirectoryError:
+        console.print(f"{file_path} is a directory and cannot be opened as a file", style="error")
+        raise Exit(1)
+
+    checksum_output = f"{checksum_type.value} checksum: {file_hash.hexdigest()} - {file_path.name}"
+
+    if output_file:
+        file_path = output_file.parent
+        if not file_path.exists():
+            file_path.mkdir(parents=True, exist_ok=True)
+
+        with open(output_file, "a") as f:
+            f.write(f"{checksum_output}\n")
+
+    if verbose:
+        console.print(checksum_output)
+
+
+def _print_successful_generation_message(verbose: bool) -> None:
+    if verbose:
+        console.print("")  # Prints a blank line between verbose output and final message
+
+    console.print("Checksums successfully generated")
+
+
+@app.command()
+def generate(
+    checksum_path: Path = Argument(
+        ..., help="Path to the directory or file for which to generate checksums", exists=True
+    ),
+    output_file: Path = Option(
+        None,
+        "--output-file",
+        "-o",
+        help="Path to the file to same the checksums. Must be a .txt file. If no path is provided the output will be printed to the screen and not saved.",
+    ),
+    checksum_type: ChecksumType = Option(
+        ChecksumType.SHA256,
+        "--checksum-type",
+        "-c",
+        help="The type of checksum to generate. [default: sha256]",
+        show_default=False,
+    ),
+    verbose: bool = Option(False, "--verbose", "-v", help="Provides more output while running"),
+) -> None:
+    if output_file:
+        try:
+            _check_valid_output_file_type(output_file)
+        except FileTypeError:
+            console.print("Error: The output file must be a .txt file", style="error")
+            raise Exit(1)
+    else:
+        verbose = True
+
+    if checksum_path.is_file():
+        _process_checksum(
+            checksum_path, output_file=output_file, checksum_type=checksum_type, verbose=verbose
+        )
+        _print_successful_generation_message(verbose)
+        raise Exit()
+
+    with console.status("Generating checksums"):
+        for file_path in checksum_path.iterdir():
+            if file_path.is_file():
+                _process_checksum(
+                    file_path,
+                    output_file=output_file,
+                    checksum_type=checksum_type,
+                    verbose=verbose,
+                )
+
+    _print_successful_generation_message(verbose)
+    raise Exit()
+
+
+@app.callback(invoke_without_command=True)
+def main(
+    version: Union[bool, None] = Option(
+        None,
+        "--version",
+        "-v",
+        is_eager=True,
+        help="Show the installed version",
+    ),
+) -> None:
+    if version:
+        console.print(__version__)
+        raise Exit()
+
+
+if __name__ == "__main__":  # pragma: no cover
+    raise SystemExit(app())
---
+++ tests/test_main.py
@@ -0,0 +1,179 @@
+from hashlib import md5, sha1, sha256
+from pathlib import Path
+
+import pytest
+from typer import Exit
+from typer.testing import CliRunner
+
+from file_checksum.__main__ import (
+    FileTypeError,
+    __version__,
+    _check_valid_output_file_type,
+    _process_checksum,
+    app,
+)
+
+try:
+    import tomli as tomllib  # type: ignore
+except ModuleNotFoundError:
+    import tomllib  # type: ignore
+
+
+def test_versions_match():
+    pyproject_file = Path().absolute() / "pyproject.toml"
+    with open(pyproject_file, "rb") as f:
+        data = tomllib.load(f)
+        pyproject_version = data["tool"]["poetry"]["version"]
+    assert __version__ == pyproject_version
+
+
+def get_checksum(file_name: Path, checksum_type: str):
+    with open(file_name, "rb") as f:
+        if checksum_type == "md5":
+            return md5(f.read())
+
+        if checksum_type == "sha1":
+            return sha1(f.read())
+
+        return sha256(f.read())
+
+
+@pytest.fixture
+def fake_file_path(tmp_path):
+    file_path = tmp_path / "test.xml"
+
+    with open(file_path, "w") as f:
+        f.write('<?xml version="1.0" encoding="UTF-8"?><test>some test text</test>')
+
+    return file_path
+
+
+@pytest.fixture
+def fake_file_path2(tmp_path):
+    file_path = tmp_path / "test.xml"
+
+    with open(file_path, "w") as f:
+        f.write('<?xml version="1.0" encoding="UTF-8"?><test>some more test text</test>')
+
+    return file_path
+
+
+def test_check_valid_output_file_type():
+    assert _check_valid_output_file_type(Path("test.txt")) is None  # type: ignore
+
+
+def test_check_valid_output_file_type_error():
+    with pytest.raises(FileTypeError):
+        _check_valid_output_file_type(Path("test.xlsx"))
+
+
+def test_process_checksum_directory_error(tmp_path):
+    with pytest.raises(Exit):
+        _process_checksum(tmp_path)
+
+
+@pytest.mark.parametrize("checksum_type", ["md5", "sha1", "sha256", None])
+@pytest.mark.parametrize("checksum_type_flag", ["-c", "--checksum-type"])
+@pytest.mark.parametrize("verbose", ["-v", "--verbose", None])
+def test_generate_file(checksum_type, checksum_type_flag, verbose, fake_file_path, tmp_path):
+    output_file = tmp_path / "output.txt"
+    checksum = get_checksum(fake_file_path, checksum_type)
+
+    args = ["generate", str(fake_file_path), "-o", str(output_file)]
+
+    if checksum_type_flag:
+        args.append(checksum_type_flag)
+        args.append(checksum_type)
+
+    if verbose:
+        args.append(verbose)
+
+    runner = CliRunner()
+    runner.invoke(app, args, catch_exceptions=False)
+
+    with open(output_file, "r") as f:
+        result = f.read()
+        assert checksum.hexdigest() in result
+
+
+def test_generate_file_creates_directory(fake_file_path, tmp_path):
+    output_file = tmp_path / "new" / "output.txt"
+    checksum = get_checksum(fake_file_path, "sha356")
+
+    args = ["generate", str(fake_file_path), "-o", str(output_file)]
+
+    runner = CliRunner()
+    runner.invoke(app, args, catch_exceptions=False)
+
+    with open(output_file, "r") as f:
+        result = f.read()
+        assert checksum.hexdigest() in result
+
+
+def test_generate_file_bad_output_file_type(fake_file_path, tmp_path):
+    output_file = tmp_path / "output.csv"
+
+    args = ["generate", str(fake_file_path), "-o", str(output_file)]
+
+    runner = CliRunner()
+    result = runner.invoke(app, args, catch_exceptions=False)
+    out = result.stdout
+
+    assert "must be a .txt file" in out
+
+
+def test_generate_file_no_output_file(fake_file_path):
+    checksum = get_checksum(fake_file_path, "sha256")
+
+    args = ["generate", str(fake_file_path)]
+
+    runner = CliRunner()
+    result = runner.invoke(app, args, catch_exceptions=False)
+    out = result.stdout
+
+    assert checksum.hexdigest() in out
+
+
+@pytest.mark.parametrize("checksum_type", ["md5", "sha1", "sha256", None])
+@pytest.mark.parametrize("checksum_type_flag", ["-c", "--checksum-type"])
+@pytest.mark.parametrize("verbose", ["-v", "--verbose", None])
+def test_generate_directory(checksum_type, checksum_type_flag, verbose, tmp_path):
+    fake_dir = tmp_path / "fake"
+    fake_dir.mkdir()
+
+    fake_file_path = fake_dir / "test.xml"
+    with open(fake_file_path, "w") as f:
+        f.write('<?xml version="1.0" encoding="UTF-8"?><test>some test text</test>')
+
+    fake_file_path2 = fake_dir / "test2.xml"
+    with open(fake_file_path2, "w") as f:
+        f.write('<?xml version="1.0" encoding="UTF-8"?><test>some more test text</test>')
+
+    output_file = tmp_path / "output.txt"
+    checksum_1 = get_checksum(fake_file_path, checksum_type)
+    checksum_2 = get_checksum(fake_file_path2, checksum_type)
+
+    args = ["generate", str(fake_dir), "-o", str(output_file)]
+
+    if checksum_type_flag:
+        args.append(checksum_type_flag)
+        args.append(checksum_type)
+
+    if verbose:
+        args.append(verbose)
+
+    runner = CliRunner()
+    runner.invoke(app, args, catch_exceptions=False)
+
+    with open(output_file, "r") as f:
+        result = f.read()
+        assert checksum_1.hexdigest() in result
+        assert checksum_2.hexdigest() in result
+
+
+@pytest.mark.parametrize("args", [["--version"], ["-v"]])
+def test_version(args):
+    runner = CliRunner()
+    result = runner.invoke(app, args, catch_exceptions=False)
+    out = result.stdout
+    assert __version__ in out
